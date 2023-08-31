3042231def2a63e6d81eccdbfdf033b78fa37b51
Ravikumar <127386201+awsravi@users.noreply.github.com>
2023-08-31 08:39:35 +0530

    Update Count1s0sUsingstreams.java

--- Count1s0sUsingstreams.java
+++ Count1s0sUsingstreams.java
@@ -7,11 +7,20 @@ public class Count1s0sUsingstreams {
 
 	public static void main(String[] args) {
 	String str="1,1,0,0,1,0,1,0,1";
-	
+	//string way
 	long out1=str.chars().filter( f -> f=='1').count();
 	long oout2=str.chars().filter(o -> o=='0').count();
 	System.out.println("count 1s :"+out1 +" : "+ "count 0s"+ oout2);
-	
+	//List of Integers 
+	List<Integer> lint=Arrays.asList(1,1,0,0,1,0,1,0,1);
+	Map<Integer, Long> counts=lint.stream().collect(Collectors.groupingBy(Integer::intValue, Collectors.counting()));
+	System.out.println(counts);
+
+
+
+
+
+		
 		//wrong just trickky way only 0s and 1s
 		Integer[] arr= {1,0,1,0,0,1,1,1,0};
 		List<Integer> list=Arrays.asList(arr);
